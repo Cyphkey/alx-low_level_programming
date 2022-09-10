dc2205f46708123d110e9ccdbc9183e026292786
Cyphkey <king4real001@gmail.com>
2022-09-10 05:40:07 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,10 @@
 /**
  *main - program that prints the alphabet in lowercase, followed by a new line
  *You can only use the putchar
- *Return: 0
+ * Return: 0
  */
-
 int main(void)
+
 {
 	int l = 'a';
 	while (l <= 'z')
