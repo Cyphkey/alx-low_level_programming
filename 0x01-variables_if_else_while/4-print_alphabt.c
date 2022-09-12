ad360e74d0c2b05978769e7244d2d1b62c245be6
Cyphkey <king4real001@gmail.com>
2022-09-12 05:34:02 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - program that prints the alphabet in lowercase, followed by a new line
+ main - program that prints the alphabet in lowercase, followed by a new line
  *You can only use the putchar
  *Return: 0
  */
@@ -11,16 +11,12 @@ int main(void)
 	int lw = 'a';
 	while (lw <= 'z')
 
-
 	{
 		if (lw == 'e' || lw == 'q')
-
 		{
 			lw += 1;
 		}
-
 		else
-
 		{
 			putchar(lw);
 			lw += 1;
